e4782666ffb9c0d30b62787df1b50ffce252efe9
Robyn Noble <robynhurd09@gmail.com>
2021-05-24 15:59:47 -0500

    playing with material ui as I learn it from a tutorial video

--- src/components/BrowseFurniture/BrowseFurniture.jsx
+++ src/components/BrowseFurniture/BrowseFurniture.jsx
@@ -1,29 +1,82 @@
 import { Typography, AppBar, Card, CardActions, 
-    CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } 
+    CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container, Button } 
     from '@material-ui/core';
-    import WeekendTwoToneIcon from '@material-ui/icons/WeekendTwoTone';
+import WeekendTwoToneIcon from '@material-ui/icons/WeekendTwoTone';
+
+import useStyles from './Styles';
+
 
 function BrowseFurniture() {
+    const classes = useStyles();
+    const cards = [1, 2, 3, 4, 5];
+
     return(
         <>
             <CssBaseline />
             <AppBar position="relative" >
                 <Toolbar>
-                    <WeekendTwoToneIcon />
+                    <WeekendTwoToneIcon className={classes.icon} />
                     <Typography variant="h6">
                         Available Furniture
                     </Typography>
                 </Toolbar>
             </AppBar>
             <main>
-                <div>
-                    <Container maxWidth="sm">
+                <div className={classes.container}>
+                    <Container maxWidth="sm" >
                         <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
                             Available Furniture
                         </Typography>
+                        <Typography variant="h5" align="center" color="textSecondary" paragraph>
+                            {/* Cards with furniture will go here */}
+                        </Typography>
+                        <div className={classes.button}>
+                            <Grid container spacing={2} justify="center">
+                                <Grid item>
+                                    <Button variant="contained" color="primary">
+                                        See my items
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth="md" >
+                    <Grid container spacing={4}>
+                        {cards.map((card) => {
+                        <Grid item key={card} xs={12} sm={6} md={4} >
+                            <Card className={classes.card}>
+                                <CardMedia 
+                                    className={classes.cardMedia}
+                                    image="https://source.unsplash.com/random"
+                                    title="Image title"
+                                />
+                                <CardContent className={classes.cardContent}>
+                                    <Typography gutterBottom variant="h5">
+                                        Heading
+                                    </Typography>
+                                    <Typography>
+                                        This is a media card. You can use this section to describe the content.
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small" color="primary">View</Button>
+                                    <Button size="small" color="primary">Edit</Button>
+                                </CardActions>
+                            </Card>
+                        </Grid>
+                        })}
+                    </Grid>
+                </Container>
             </main>
+            <footer className={classes.footer}>
+                <Typography variant="h6" align="center" gutterBottom>
+                    Footer
+                </Typography>
+                <Typography variant="subtitle1" align="center" color="textSecondary">
+                        Somthing here to give footer a purpose
+                </Typography>
+            </footer>
         </>
     )
 }
