9728d9ed0fc434eb81dfaef30755b58ad507f845
Robyn Noble <robynhurd09@gmail.com>
2021-05-24 15:05:01 -0500

    adding style to browse furniture page, while learning material ui

--- src/components/BrowseFurniture/BrowseFurniture.jsx
+++ src/components/BrowseFurniture/BrowseFurniture.jsx
@@ -1,5 +1,31 @@
+import { Typography, AppBar, Card, CardActions, 
+    CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } 
+    from '@material-ui/core';
+    import WeekendTwoToneIcon from '@material-ui/icons/WeekendTwoTone';
+
 function BrowseFurniture() {
-    
+    return(
+        <>
+            <CssBaseline />
+            <AppBar position="relative" >
+                <Toolbar>
+                    <WeekendTwoToneIcon />
+                    <Typography variant="h6">
+                        Available Furniture
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <main>
+                <div>
+                    <Container maxWidth="sm">
+                        <Typography variant="h2" align="center" color="textPrimary" gutterBottom>
+                            Available Furniture
+                        </Typography>
+                    </Container>
+                </div>
+            </main>
+        </>
+    )
 }
 
 export default BrowseFurniture;
