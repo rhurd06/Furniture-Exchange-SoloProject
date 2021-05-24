266a236e865e02b99434ee9cc5a6b3ce2c279ab6
Robyn Noble <robynhurd09@gmail.com>
2021-05-24 11:39:33 -0500

    added additional params to registration form

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,6 +19,7 @@ import InfoPage from '../InfoPage/InfoPage';
 import LandingPage from '../LandingPage/LandingPage';
 import LoginPage from '../LoginPage/LoginPage';
 import RegisterPage from '../RegisterPage/RegisterPage';
+import BrowseFurniture from '../BrowseFurniture/BrowseFurniture';
 
 import './App.css';
 
@@ -44,6 +45,14 @@ function App() {
             path="/about"
           >
             <AboutPage />
+          </Route>
+
+          <Route
+          //to Browse furniture
+          exact
+          path="/browseFurniture"
+          >
+            <BrowseFurniture />
           </Route>
 
           {/* For protected routes, the view could show one of several things on the same route.
---
+++ src/components/BrowseFurniture/BrowseFurniture.jsx
@@ -0,0 +1,5 @@
+function BrowseFurniture() {
+    
+}
+
+export default BrowseFurniture;
