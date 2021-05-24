8dda49bbe9fd52a1bfbb40fdc37535fda6e2eacb
Robyn Noble <robynhurd09@gmail.com>
2021-05-24 10:54:52 -0500

    database updated

--- database.sql
+++ database.sql
@@ -1,10 +1,36 @@
 
--- USER is a reserved keyword with Postgres
--- You must use double quotes in every query that user is in:
--- ex. SELECT * FROM "user";
--- Otherwise you will have errors!
+-- -- USER is a reserved keyword with Postgres
+-- -- You must use double quotes in every query that user is in:
+-- -- ex. SELECT * FROM "user";
+-- -- Otherwise you will have errors!
+-- CREATE TABLE "user" (
+--     "id" SERIAL PRIMARY KEY,
+--     "username" VARCHAR (80) UNIQUE NOT NULL,
+--     "password" VARCHAR (1000) NOT NULL
+-- );
+
+--CREATE DATABASE called "furniture-exchange"
+-- Tables you need for base mode
 CREATE TABLE "user" (
-    "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
-);
+	"id" SERIAL PRIMARY KEY,
+	"username" VARCHAR (80) UNIQUE NOT NULL,
+	"password" VARCHAR (1000) NOT NULL,
+	"email" VARCHAR (1000)UNIQUE NOT NULL,
+	"phone_number" integer
+	);
+	
+CREATE TABLE "furniture" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" int REFERENCES "user",
+	"picture_url" VARCHAR (5000),
+	"description" VARCHAR (5000),
+	"furniture_type" VARCHAR (200),
+	"sold" BOOLEAN DEFAULT FALSE
+	);
+	
+CREATE TABLE "favorites" (
+	"id" SERIAL PRIMARY KEY,
+	"user_id" int REFERENCES "user",
+	"furniture_id" int REFERENCES "furniture"
+	);
+	
