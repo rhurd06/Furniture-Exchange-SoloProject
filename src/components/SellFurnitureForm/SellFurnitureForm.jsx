a973c88d70595a6b758a5c0a423dedf5f5adc0ff
Robyn Noble <robynhurd09@gmail.com>
2021-05-25 11:41:04 -0500

    states created in sellFurnitureForm

--- src/components/SellFurnitureForm/SellFurnitureForm.jsx
+++ src/components/SellFurnitureForm/SellFurnitureForm.jsx
@@ -6,6 +6,23 @@ const SellFurnitureForm = () => {
     const dispatch = useDispatch();
     const history = useHistory();
 
+    const [imageUrl, setImageUrl] = useState('');
+    const [cost, setCost] = useState('');
+    const [location, setLocation] = useState('');
+    const [description, setDescription] = useState('');
+    const [preferredContact , setPreferredContact] = useState('');
+    const [username, setUsername] = useState('');
+
+    const submitForm = (event) => {
+        event.preventDefault();
+        console.log('Clicked submit form');
+        dispatch({ type: 'POST_BROWSE_FURNITURE', imageUrl: imageUrl, cost: cost});
+        dispatch({ type: 'POST_FURNITURE', imageUrl: imageUrl, cost: cost, 
+                location: location, description: description, 
+                preferredContact: preferredContact, username: username });
+        // history.push('/itemView');
+    }
+
     return(
 
     );
