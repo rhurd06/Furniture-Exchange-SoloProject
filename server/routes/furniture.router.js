54d286ec4f0e3adc4c312d156b5ebbea856bbaa4
Robyn Noble <robynhurd09@gmail.com>
2021-05-25 11:17:40 -0500

    created a get route in furniture router

--- server/routes/furniture.router.js
+++ server/routes/furniture.router.js
@@ -3,5 +3,16 @@ const pool = require('../modules/pool');
 const router = express.Router();
 const { rejectUnauthenticated } = require('../modules/authentication-middleware');
 
+// get all the furniture items from the database
+router.get('/', (req, res) => {
+    pool.query (`SELECT * FROM "furniture";`)
+        .then((results) => {
+            res.send(results.rows);
+        })
+        .catch((error) => {
+            res.sendStatus(500);
+            console.log('Error in GET furniture', error);
+        })
+});
 
 module.exports = router;
