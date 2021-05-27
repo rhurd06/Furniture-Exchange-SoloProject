bf7e36934dba4e07ccbf32a72d52be0222ef479c
Robyn Noble <robynhurd09@gmail.com>
2021-05-27 11:24:42 -0500

    added a delete router to myItems router

--- server/routes/myItems.router.js
+++ server/routes/myItems.router.js
@@ -29,6 +29,18 @@ router.put('/:id', rejectUnauthenticated, (req, res) => {
             console.log(`Error making database query ${query}`, error);
             res.sendStatus(500);
         });
-})
+});
+
+//delete one of my items
+router.delete('/:id', rejectUnauthenticated, (req, res) => {
+    const query = `DELETE FROM "furniture" WHERE id=$1 AND user_id=$2;`;
+    pool.query(query, [req.params.id, req.user.id])
+        .then(() => { res.sendStatus(201) 
+        })
+        .catch(error => {
+            console.log('Error deleting item', error);
+            res.sendStatus(500);
+        });
+});
 
 module.exports = router;
