0ded7eb39e3d4c8e47a5fea28fd1b95141f87f61
Robyn Noble <robynhurd09@gmail.com>
2021-09-23 13:51:21 -0500

    updated styles

--- src/components/BrowseFurniture/BrowseFurniture.jsx
+++ src/components/BrowseFurniture/BrowseFurniture.jsx
@@ -37,7 +37,7 @@ function BrowseFurniture() {
                     </Typography>
                 </Toolbar>
             </AppBar>
-            <Grid container spacing={4}>
+            <Grid style={{backgroundColor: "lightseagreen"}} container spacing={4}>
                 {furniture.map((furniture, i) => {
                     return  <Grid item key={furniture.id} xs={12} sm={6} md={4} >
                         <FurnitureCardTemplate furniture={furniture} i={i} />
